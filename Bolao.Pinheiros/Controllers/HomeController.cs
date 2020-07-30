2022b071b0427c40d29df9f9e81e7072d3f1396f
danillopneto <danillopneto@outlook.com>
2020-07-30 19:05:09 -0300

    Ajuste na ordenação dos últimos jogos. Remoção de competições.

--- Bolao.Pinheiros/Controllers/HomeController.cs
+++ Bolao.Pinheiros/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace Bolao.Pinheiros.Controllers
         #region " CONSTANTS "
 
         private static readonly string DATE_FORMAT = "dd/MM/yyyy";
-        private static readonly List<int> EXCLUDE_COMPETITIONS = new List<int> { 165, 173, 397, 564, 5431, 5456, 5547, 5565, 5556, 5605, 5606, 5639, 5807, 5825, 6219, 6362, 6896, 7298, 7299, 7345, 7499, 7522, 7523, 7528, 7529, 7530, 7538, 7540, 7556, 7568, 7569, 7570, 7572 };
+        private static readonly List<int> EXCLUDE_COMPETITIONS = new List<int> { 165, 173, 397, 564, 5431, 5456, 5547, 5565, 5556, 5605, 5606, 5639, 5807, 5818, 5825, 6219, 6345, 6362, 6896, 7110, 7298, 7299, 7345, 7499, 7522, 7523, 7528, 7529, 7530, 7538, 7540, 7556, 7568, 7569, 7570, 7572 };
         private static readonly string GAMES_DATA = "GamesData";
         private static readonly int MAXIMUM_GAMES = 5;
         private static readonly string URL_BASE = "https://webws.365scores.com/web/games/?langId=31&timezoneName=America/Sao_Paulo&userCountryId=21&appTypeId=5&sports=1&startDate={0}&endDate={1}&showOdds=true";
